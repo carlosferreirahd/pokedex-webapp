d146a2cdad24662739f3b4325f7237998edcc1fa
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-03-29 16:00:17 -0300

    feat: app layout with header/main/footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 function App() {
 
   return (
-    <h1 className="text-red-500">
-      Pokédex Webapp
-    </h1>
+    <div className="w-full min-h-screen flex flex-col">
+      <header className="w-full shadow-md">
+        <div className="container">
+          header
+        </div>
+      </header>
+
+      <main className="flex flex-1">
+        <div className="container py-8">
+          main content
+        </div>
+      </main>
+
+      <footer className="w-full bg-base-300 text-base-content">
+        <div className="container">
+          footer
+        </div>
+      </footer>
+    </div>
   );
 }
 
