4017e571e9cd1468250b5caab286d7fe1c877c96
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-03-29 15:43:06 -0300

    feat: added path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+interface Alias {
+  [key: string]: string;
+}
+
+const alias = (path = "src") => ({
+  "@components": `${path}/components`,
+});
+
+const resolvedAlias = (alias: Alias): Record<string, string> =>
+  Object.entries(alias).reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: path.resolve(__dirname, value),
+    }),
+    {}
+  );
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: resolvedAlias(alias()),
+  },
   plugins: [react()],
-})
+});
