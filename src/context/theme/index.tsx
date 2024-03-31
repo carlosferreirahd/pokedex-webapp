87ec46efeae9ba79e0a50f5ed1600c710cc3d7d6
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-03-31 12:16:01 -0300

    feat: ThemeProvider to handle dark/light/system themes change

---
+++ src/context/theme/index.tsx
@@ -0,0 +1,85 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import {
+  getFromStorage,
+  saveToStorage,
+} from "@utils/storage";
+
+type Theme = "light" | "dark";
+
+interface IThemeContextData {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+interface ThemeProviderProps extends React.PropsWithChildren { }
+
+const ThemeContextData: IThemeContextData = {
+  theme: "light",
+  toggleTheme: () => null,
+};
+
+const ThemeContext = createContext(ThemeContextData);
+
+const STORAGE_THEME_KEY = "@theme-storage";
+
+function ThemeProvider({
+  children,
+}: ThemeProviderProps) {
+
+  const [theme, setTheme] = useState<Theme>(() => {
+    const themeFromStorage = getFromStorage(STORAGE_THEME_KEY) as Theme;
+
+    if (themeFromStorage) {
+      if (themeFromStorage === "dark") return "dark";
+      return "light";
+    }
+
+    const preferredTheme = window?.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+
+    return preferredTheme;
+  });
+
+  const toggleTheme = useCallback(() => {
+    setTheme(currentTheme => {
+      if (currentTheme === "light") return "dark";
+      return "light";
+    });
+  }, []);
+
+  useEffect(() => {
+    saveToStorage(STORAGE_THEME_KEY, theme);
+  }, [theme]);
+
+  const providerValue = useMemo<IThemeContextData>(() => ({
+    theme,
+    toggleTheme,
+  }), [theme, toggleTheme]);
+
+  return (
+    <ThemeContext.Provider value={providerValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error('useTheme must' +
+      ' be used within a ThemeProvider');
+  }
+
+  return context;
+}
+
+export { ThemeProvider, useTheme };
