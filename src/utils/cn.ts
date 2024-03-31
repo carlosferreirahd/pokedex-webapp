ed30437121a85463cf6b0625bf3caf31335f4338
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-03-31 12:29:21 -0300

    feat: using tailwind-merge and clsx to join classNames

---
+++ src/utils/cn.ts
@@ -0,0 +1,6 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export default function cn(...classNames: ClassValue[]) {
+  return twMerge(clsx(classNames));
+}
