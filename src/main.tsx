4591eedcd6851be8329dc143343c0bba3a3954b6
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-04-02 17:27:10 -0300

    feat: install tanstack query to handle api calls

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from '@context/theme';
 import { ErrorBoundary } from '@components/layout';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </ErrorBoundary>
   </React.StrictMode>,
 );
