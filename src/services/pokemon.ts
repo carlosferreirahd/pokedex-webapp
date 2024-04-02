b43f23480d9f4644f25decd1a531b5c0581865e4
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-04-02 17:27:40 -0300

    feat: adds paginated pokemon list service function

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -3,6 +3,20 @@ import api from "@services/api";
 function service() {
   const resource = "/pokemon";
 
+  async function getPaginatedPokemonList(
+    offset: number = 0,
+    limit: number = 20
+  ) {
+    try {
+      const response = await api.get(
+        `${resource}/?offset=${offset}&limit=${limit}`
+      );
+      return response.data;
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
   async function getPokemonByName(pokemonName: string) {
     try {
       const response = await api.get(`${resource}/${pokemonName}`);
@@ -13,6 +27,7 @@ function service() {
   }
 
   return {
+    getPaginatedPokemonList,
     getPokemonByName,
   };
 }
