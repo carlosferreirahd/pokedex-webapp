00b4081ffcf4a9b97cf7616384f53495682e2b49
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-04-03 20:34:44 -0300

    feat: component to render pokemon type badge

---
+++ src/components/pokemon-badge/index.tsx
@@ -0,0 +1,41 @@
+import cn from "@utils/cn";
+import { PokemonType } from "@shared/interfaces/pokemon.interface";
+
+interface PokemonBadgeProps {
+  pokemonType: PokemonType
+}
+
+export function PokemonBadge({
+  pokemonType,
+}: Readonly<PokemonBadgeProps>) {
+
+  return (
+    <div
+      className={cn(
+        "badge text-xs text-base-200 font-semibold opacity-100",
+        {
+          "bg-normal": pokemonType === "normal",
+          "bg-fire": pokemonType === "fire",
+          "bg-water": pokemonType === "water",
+          "bg-electric": pokemonType === "electric",
+          "bg-grass": pokemonType === "grass",
+          "bg-ice": pokemonType === "ice",
+          "bg-fighting": pokemonType === "fighting",
+          "bg-poison": pokemonType === "poison",
+          "bg-ground": pokemonType === "ground",
+          "bg-flying": pokemonType === "flying",
+          "bg-psychic": pokemonType === "psychic",
+          "bg-bug": pokemonType === "bug",
+          "bg-rock": pokemonType === "rock",
+          "bg-ghost": pokemonType === "ghost",
+          "bg-dragon": pokemonType === "dragon",
+          "bg-dark": pokemonType === "dark",
+          "bg-steel": pokemonType === "steel",
+          "bg-fairy": pokemonType === "fairy",
+        }
+      )}
+    >
+      {pokemonType}
+    </div>
+  );
+}
