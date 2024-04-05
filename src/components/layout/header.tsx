cce20473071e481f82ac84466869425f4130cb6b
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-04-04 21:25:48 -0300

    refactor: treating header responsiveness

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,10 +3,79 @@ import MoonIcon from "@components/ui/icons/moon";
 import PokeballIcon from "@components/ui/icons/pokeball";
 import SunIcon from "@components/ui/icons/sun";
 import { useTheme } from "@context/theme";
+import cn from "@utils/cn";
 
-export function Header() {
+function Search() {
+
+  return (
+    <form className={cn(
+      "form-control w-full",
+      "lg:w-auto"
+    )}>
+      <label
+        htmlFor="pokemon-search"
+        className="input input-primary input-bordered flex items-center gap-2"
+      >
+        <MagnifyIcon
+          className="size-5 mb-1 inline-block align-middle"
+        />
+        <input
+          id="pokemon-search"
+          type="text"
+          className={cn(
+            "grow w-full",
+            "sm:w-auto"
+          )}
+          placeholder="Search..."
+        />
+        <kbd className={cn(
+          "kbd kbd-sm hidden",
+          "lg:flex"
+        )}>
+          Ctrl
+        </kbd>
+        <kbd className={cn(
+          "kbd kbd-sm hidden",
+          "lg:flex"
+        )}>
+          K
+        </kbd>
+      </label>
+    </form>
+  );
+}
+
+function ThemeSwitch() {
 
   const { toggleTheme, theme } = useTheme();
+
+  return (
+    <label
+      htmlFor="theme-switch"
+      className="swap swap-rotate btn btn-ghost focus-within:bg-base-200"
+    >
+      <input
+        tabIndex={0}
+        id="theme-switch"
+        className="theme-controller"
+        type="checkbox"
+        checked={theme === "light"}
+        aria-label="Switch between Light and Dark themes"
+        onChange={toggleTheme}
+      />
+      <SunIcon
+        className="swap-on fill-current size-7"
+        aria-label="Light theme"
+      />
+      <MoonIcon
+        className="swap-off fill-current size-7"
+        aria-label="Dark theme"
+      />
+    </label>
+  );
+}
+
+export function Header() {
 
   return (
     <header className="w-full shadow-md">
@@ -14,49 +83,27 @@ export function Header() {
         <div className="navbar bg-base-100 py-3">
           <div className="navbar-start">
             <h1 className="font-semibold">
-              <PokeballIcon className="inline-block align-middle size-7 mr-2 mb-1" />
+              <PokeballIcon
+                className="inline-block align-middle size-7 mr-2 mb-1"
+              />
               Pokédex
             </h1>
           </div>
-          <div className="navbar-center">
-            <form className="form-control">
-              <label htmlFor="pokemon-search" className="input input-primary input-bordered flex items-center gap-2">
-                <MagnifyIcon className="size-5 mb-1 inline-block align-middle" />
-                <input
-                  id="pokemon-search"
-                  type="text"
-                  className="grow"
-                  placeholder="Search..."
-                />
-                <kbd className="kbd kbd-sm">Ctrl</kbd>
-                <kbd className="kbd kbd-sm">K</kbd>
-              </label>
-            </form>
+          <div className={cn(
+            "hidden",
+            "sm:flex sm:navbar-center",
+          )}>
+            <Search />
           </div>
           <div className="navbar-end">
-            <label
-              htmlFor="theme-switch"
-              className="swap swap-rotate btn btn-ghost focus-within:bg-base-200"
-            >
-              <input
-                tabIndex={0}
-                id="theme-switch"
-                className="theme-controller"
-                type="checkbox"
-                checked={theme === "light"}
-                aria-label="Switch between Light and Dark themes"
-                onChange={toggleTheme}
-              />
-              <SunIcon
-                className="swap-on fill-current size-7"
-                aria-label="Light theme"
-              />
-              <MoonIcon
-                className="swap-off fill-current size-7"
-                aria-label="Dark theme"
-              />
-            </label>
+            <ThemeSwitch />
           </div>
+        </div>
+        <div className={cn(
+          "w-full flex justify-center pb-4",
+          "sm:hidden"
+        )}>
+          <Search />
         </div>
       </div>
     </header>
