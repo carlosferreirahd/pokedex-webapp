06397b5c4193e9a5722f1521cf5ad63cd4f75b15
Carlos Ferreira <carlosferreirahd@gmail.com>
2024-04-10 20:19:22 -0300

    feat: background color support using lighter types colors for sprite component

--- src/components/pokemon-card/sprite.tsx
+++ src/components/pokemon-card/sprite.tsx
@@ -1,10 +1,16 @@
 import PhotoIcon from "@components/ui/icons/photo";
+import cn from "@utils/cn";
+import { PokemonType } from "@shared/interfaces/pokemon.interface";
 
-interface PokemonSpriteProps extends React.ImgHTMLAttributes<HTMLImageElement> { }
+interface PokemonSpriteProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  maskType?: PokemonType
+}
 
 export function PokemonSprite({
+  className,
+  maskType,
   ...props
-}: PokemonSpriteProps) {
+}: Readonly<PokemonSpriteProps>) {
 
   if (!props.src) {
     return (
@@ -18,6 +24,29 @@ export function PokemonSprite({
     <figure>
       <img
         {...props}
+        className={cn(
+          className,
+          {
+            "bg-normal-lighter": maskType === "normal",
+            "bg-fire-lighter": maskType === "fire",
+            "bg-water-lighter": maskType === "water",
+            "bg-electric-lighter": maskType === "electric",
+            "bg-grass-lighter": maskType === "grass",
+            "bg-ice-lighter": maskType === "ice",
+            "bg-fighting-lighter": maskType === "fighting",
+            "bg-poison-lighter": maskType === "poison",
+            "bg-ground-lighter": maskType === "ground",
+            "bg-flying-lighter": maskType === "flying",
+            "bg-psychic-lighter": maskType === "psychic",
+            "bg-bug-lighter": maskType === "bug",
+            "bg-rock-lighter": maskType === "rock",
+            "bg-ghost-lighter": maskType === "ghost",
+            "bg-dragon-lighter": maskType === "dragon",
+            "bg-dark-lighter": maskType === "dark",
+            "bg-steel-lighter": maskType === "steel",
+            "bg-fairy-lighter": maskType === "fairy",
+          }
+        )}
         alt={props.alt}
         loading="lazy"
       />
